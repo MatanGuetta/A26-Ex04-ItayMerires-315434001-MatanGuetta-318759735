2c2099daf23b5b2d5619a0f652f8779b406fcb74
Matan Guetta <matangu2@mta.ac.il>
2026-01-22 14:42:04 +0200

    ...

--- Ex04.Menus.Test/obj/Debug/net8.0/Ex04.Menus.Test.AssemblyInfo.cs
+++ Ex04.Menus.Test/obj/Debug/net8.0/Ex04.Menus.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ex04.Menus.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34d5a4669826868777d1b190a0c697320b2fdcb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1847b73f4ac5820abda994e3b3f5aadb3cccb00")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ex04.Menus.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ex04.Menus.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
